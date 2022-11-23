27d9d4de719152bafdf9f920d15bd15bc3290115
Nazar Marusyn <marusinnazar@gmail.com>
2022-11-23 17:18:39 +0200

    Update stat func

--- api/stat.py
+++ api/stat.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from database.crud import get_top_urls_by_clicks
 from database.database import get_db
 
+from app.database.schemas import Url
+
 router = fastapi.APIRouter()
 
 
@@ -12,6 +14,6 @@ async def top_urls(urls_count: int = 10, db: Session = Depends(get_db)):
     if urls_count < 1:
         raise HTTPException(status_code=400, detail="urls_count should be greater than 0")
     result = get_top_urls_by_clicks(db, urls_count)
-    return result
+    return [Url(url_model=url_model) for url_model in result]
 
 
